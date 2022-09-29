842a0225fc316b838f7d7a915f5ffaead74f05e0
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-29 14:33:28 +0500

    added example001

---
+++ Example001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string GetNaturalNumbersToN(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+
+    string result = GetNaturalNumbersToN(n - 1) + " " + n;
+    return result;
+}
+
+Console.WriteLine("Введите значение для числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = GetNaturalNumbersToN(n);
+
+Console.WriteLine($"Натуральные числа в промежутке от 1 до {n}: ");
+Console.WriteLine($"{result}");
